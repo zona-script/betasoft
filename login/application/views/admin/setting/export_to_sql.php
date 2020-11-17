df256662118551353806ec9fa89b5fcbd155bb66
anandu194 <anandu194@gmail.com>
2020-11-17 10:55:31 +0530

    added export to sql

---
+++ login/application/views/admin/setting/export_to_sql.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<div class="row">
+    <div align="center">
+        <div class="fa fa-database" style="font-size: 100px"></div>
+        <br/>
+        <p>
+            <a href="<?php echo site_url('setting/export_final') ?>" class="btn btn-danger"> Export to SQL &rarr;</a>
+        </p>
+    </div>
+</div>
