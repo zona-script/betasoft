551e95b408b0bc998bff59f8186ce3f6420dc1e1
anandu194 <anandu194@gmail.com>
2020-11-17 11:01:33 +0530

    added main screen

---
+++ login/application/views/admin/setting/main_screen.php
@@ -0,0 +1,61 @@
+<?php
+
+?>
+<div class="row">
+    <div class="state-overview">
+        <a href="<?php echo site_url('setting/common-setting') ?>">
+            <div class="col-sm-3">
+                <div class="panel bg-green-400">
+                    <div class="symbol">
+                        <i class="fa fa-cog usr-clr"></i>
+                    </div>
+                    <div class="panel-body">
+                        <p class="sbold addr-font-h2">Common</p>
+                        <p>Setting</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="<?php echo site_url('setting/advance-setting') ?>">
+            <div class="col-sm-3">
+                <div class="panel bg-orange-400">
+                    <div class="symbol">
+                        <i class="fa fa-wrench usr-clr"></i>
+                    </div>
+                    <div class="panel-body">
+                        <p class="sbold addr-font-h2">Advance</p>
+                        <p>Setting</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="<?php echo site_url('setting/payout-setting') ?>">
+            <div class="col-sm-3">
+                <div class="panel bg-teal-400">
+                    <div class="symbol">
+                        <i class="fa fa-usd usr-clr"></i>
+                    </div>
+                    <div class="panel-body">
+                        <p class="sbold addr-font-h2">Payout</p>
+                        <p>Setting</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo site_url('setting/payment-gateway') ?>">
+            <div class="col-sm-3">
+                <div class="panel bg-blue-400">
+                    <div class="symbol">
+                        <i class="fa fa-yen usr-clr"></i>
+                    </div>
+                    <div class="panel-body">
+                        <p class="sbold addr-font-h2">Payment</p>
+                        <p>Gateway</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
